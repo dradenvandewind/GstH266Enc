d61dad5cd2e558b9e975b8821cd3a81d2d672f08
le blond erwan <erwanleblond@gmail.com>
2025-10-17 11:08:57 +0200

    add some test script

---
+++ vvc_test_h266_10_bit_file.sh
@@ -0,0 +1,2 @@
+#GST_DEBUG=4   gst-launch-1.0 videotestsrc ! video/x-raw,format=I420_10LE, width=256,height=256 ! h266enc bitrate=1000000 ! filesink   location=videotestsrc.266
+gst-launch-1.0 filesrc location="../akiyo_cif.y4m" ! y4mdec ! videoconvert ! video/x-raw,width=352,height=288,format=I420_10LE ! identity silent=false ! h266enc bitrate=1000000 ! filesink location=../vvc.266
